8c0cf458a0565421355e1ff772c345e3c4ec663f
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-14 09:45:25 +0000

    Added script in package.json called "dev" to run the server using nodemon

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ const mongoose = require('mongoose');
 const app = express();
 
 // MongoDB connection
+// We need to create an instace in MongoDB and get the Url for connecting to the Cluster
 const mongoUri = 'mongodb+srv://...'
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
+// Success connection notification - Mongoose
 mongoose.connection.on('connected', ()=> {
     console.log('Connected to Mongo Instance!');
 });
 
+// Error connection notification - Mongoose
 mongoose.connection.on('error', err =>{
     console.log('Something went wrong with Mongo instance connection: ', err);
 });
