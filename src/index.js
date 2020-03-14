2d03da411adf79b43aded8bcafaa6a9f1fec5a17
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-14 09:39:39 +0000

    MongoDB configuration with Mongoose.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// MongoDB connection
+const mongoUri = 'mongodb+srv://...'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', ()=> {
+    console.log('Connected to Mongo Instance!');
+});
+
+mongoose.connection.on('error', err =>{
+    console.log('Something went wrong with Mongo instance connection: ', err);
+});
+
+// Routing
 app.get('/', (req, res) => {
     res.send('Testing routing');
 });
 
+// Opening port for listening connections / requests
 app.listen(3000, ()=>{
     console.log('Listening on port: 3000');
 });
