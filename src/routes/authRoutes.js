d9dd0041ce08604dc2c5fc6adf56334aac515871
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-15 11:20:29 +0000

    Added handler - POST request for Signup a new user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,8 +4,13 @@ const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signin', (req, res) => {
-    res.send('POST request has been made');
+// Async Function for Signup a new User
+router.post('/signup', async (req, res) => {
+   const { email, password } = req.body;
+
+   const user = new User({ email, password });
+   await user.save();
+
 });
 
 module.exports = router;
