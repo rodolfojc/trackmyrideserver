e152cda7b0626ffdc4558dcfb7104da6d7f8a4f6
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-17 11:51:55 +0000

    Signin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,7 @@ const User = mongoose.model('Users');
 
 const router = express.Router();
 
-// Async Function for Signup a new User
+// Async Function for Signup a new user
 router.post('/signup', async (req, res) => {
    const { email, password } = req.body;
 
@@ -18,6 +18,39 @@ router.post('/signup', async (req, res) => {
 
    } catch (err) {
     return res.status(422).send(err.message);
+   }
+});
+
+// Async Function for Signin an existing user
+router.post('/signin', async (req, res) => {
+   
+   // Destructuring body content from request 
+   const { email, password } = req.body;
+
+   // Checking if email and paswword is provided
+   if (!email || !password) {
+      return res.status(422).send({ error : 'You must provide an email and password' });
+   }
+
+   // Checking if the user is saved in database
+   const user = await User.findOne({ email });
+   
+   // Not email found
+   if (!user) {
+      return res.status(422).send({ error: 'Invalid password or email' });
+   }
+
+   // Email found
+   try {
+      // Comparing passwords; user input + salt + hash === hashed password in database
+      await user.comparePassword(password);
+      // Generating JSON Web Token
+      const token = jwt.sign({ userId: user._id }, 'TraCKmyR1D3');
+      // Sending token to user
+      res.send({ token });
+   } catch (err) {
+      // Password did not match
+      return res.status(422).send({ error: 'Invalid password or email' });
    }
 });
 
