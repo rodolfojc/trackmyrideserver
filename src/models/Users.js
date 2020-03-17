1c7375d7d4628e06606f90ce1f54f58e9e4c16d0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-17 11:31:39 +0000

    Adding Salt and Hashing for sigin, also model method to compare password user input with hash password in DB

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// User Schema
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,5 +13,54 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+// Hashing and Salt before the user is saved in database (method - pre)
+// function as "Function to use (this) keyword pointing the current user object"
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    // Check is the password is not modified
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    // Generating a Salt with complexity 10 (characters)
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        // Hashing user password + salt (generated)
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            // Asigning the hast password to current user
+            user.password = hash;
+        });
+    });
+});
+
+// Adding a method to compare hashed password with user password input
+userSchema.methods.comparePassword = function (userPasswordInput) {
+    const user = this;
+
+    return new Promise((resolve, reject) => {
+        // Comparing password with bycryp build in method
+        bcrypt.compare(userPasswordInput, user.password, (err, isMatch) => {
+            // Any error = rejected
+            if (err) {
+                return reject(err);
+            }
+            // No match = rejected false
+            if (!isMatch) {
+                return reject(false);
+            }
+
+            // Resolve with True
+            resolve(true);
+        });
+    });
+};
 
 mongoose.model('Users', userSchema);
