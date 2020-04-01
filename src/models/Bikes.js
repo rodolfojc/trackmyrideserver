8e9cc10b59b2330bb330fd2d983b83be96d4d681
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-04-01 21:51:40 +0100

    Added - Bike Schema to register

---
+++ src/models/Bikes.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const bikeSchema = new mongoose.Schema({
+    serial: {
+        type: Number,
+    },
+    brand: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+    type: {
+        type: String,        
+    },
+    status: {
+        type: String,        
+    },
+    lock: {
+        type: Boolean
+    },
+    img: {
+        data: Buffer, 
+        contentType: String
+    }
+
+});
+
+mongoose.model('Bike', bikeSchema);
