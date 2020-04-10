8a77be1c23aa080ec110684ec0302b8a65aafbcd
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-04-10 21:28:24 +0100

    Minor change

--- src/routes/racksRoutes.js
+++ src/routes/racksRoutes.js
@@ -1,18 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Racks = mongoose.model("Racks");
+const express = require('express');
+const mongoose = require('mongoose');
+const Racks = mongoose.model('Racks');
 
 const router = express.Router();
 
 // Async function for adding a new rack
-router.post("/map", async (req, res) => {
+router.post('/addrack', async (req, res) => {
   
-  console.log(req.body);
-
+  // console.log(req.body);
   try {
     const rack = new Racks(req.body);
     await rack.save();
-    res.status(200).send({ message: "Rack Added Sucesfully" });
+    res.status(200).send({ message: "Rack has been added for review" });
+  }catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
+
+router.get('/getracks', async (req, res) =>{
+  try {
+    const racks = await Racks.find();
+    res.status(200).send(racks);
   }catch (err) {
     return res.status(422).send(err.message);
   }
