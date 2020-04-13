a9ec87733deb6a0d5e63c6e032c7a3b763e22d61
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-04-13 17:12:15 +0100

    Added action functions for adding / getting images

--- src/routes/bikeRoutes.js
+++ src/routes/bikeRoutes.js
@@ -1,34 +1,14 @@
+module.exports.UPLOAD_PATH = 'uploads';
+
 const express = require("express");
-const mongoose = require("mongoose");
-const Bike = mongoose.model("Bike");
-const imagesSchema = require('../models/Images');
+const bikeCtrl = require('../controllers/bikeController');
+const multer = require('multer');
+upload = multer(({ dest: module.exports.UPLOAD_PATH }));
+
 const router = express.Router();
-
-router.post("/registerbike", async (req, res) => {
-  const { serial, brand, color, type, status, lock } = req.body;
-
-    //console.log(req.file.name);
-    console.log(req.body.serial);
-
-    let img = new imagesSchema();
-    img.filename = req.file.filename;
-    img.originalName = req.file.originalname;
-    img.desc = req.body.desc;
-    // img.save(err => {
-    //     if (err) {
-    //         return res.sendStatus(400);
-    //     }
-    //     res.status(201).send({ newImage })
-    // });
-
-  try {
-    const bike = new Bike({ userId: req.user_id, serial, brand, color, type, status, lock, img });
-    console.log(bike);
-    await bike.save();
-    res.status(200).send({ message: "Bike has been registered" });
-  } catch (err) {
-    return res.status(422).send(err.message);
-  }
-});
+router.post("/registerbike", bikeCtrl.registerBike);
+router.post("/upload", upload.single('image'), bikeCtrl.uploadImage);
+router.get("/images", bikeCtrl.getImages);
+router.get('/images/:id', bikeCtrl.getImage);
 
 module.exports = router;
